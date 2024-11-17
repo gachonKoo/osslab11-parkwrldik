3c7050d40e1a829ca6c7005eef4b1154cb4c8905
parkwrldik <siparkgood@gmail.com>
2024-11-17 21:40:59 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,11 +1,11 @@
-import geo.utils as utils
+import geo.utils as utils  # geo.utils 모듈 가져오기
 
-# calculate the length of hypotenuse(c) when a=3 and b=4
+# 빗변 길이 계산 (a=3, b=4)
 a, b = 3, 4
-c = utils.hypotenuse(a, b)
+c = utils.hypotenuse(a, b)  # hypotenuse 함수 호출
 print('c =', c)
 
-# calculate the area of circle with radius r = 10
+# 원의 넓이 계산 (반지름 r=10)
 r = 10
-area = utils.circle_area(r)
+area = utils.circle_area(r)  # circle_area 함수 호출
 print('area =', area)
