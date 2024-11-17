89754bed41d7dd61a41b7f4063ee6ed92d955b1f
parkwrldik <siparkgood@gmail.com>
2024-11-17 21:28:03 +0900

    Update utils.py

--- geo/utils.py
+++ geo/utils.py
@@ -1,16 +1,5 @@
-import math
+def add(a, b):
+    return a + b
 
-def calculate_distance(coord1, coord2):
-    """
-    두 좌표 간의 유클리드 거리를 계산하는 함수.
-    
-    Parameters:
-    coord1, coord2: 각각 (x, y) 형식의 튜플
-    
-    Returns:
-    거리 (float)
-    """
-    x1, y1 = coord1
-    x2, y2 = coord2
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return distance
+def subtract(a, b):
+    return a - b
